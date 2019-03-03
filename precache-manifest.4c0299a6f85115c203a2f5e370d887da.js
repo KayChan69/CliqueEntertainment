3f2eda763077336461d483248b7dd6fabfc27980
unknown <keanuchan@live.com>
2019-03-03 01:53:34 -0500

    Updates

---
+++ precache-manifest.4c0299a6f85115c203a2f5e370d887da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bc3632eab9e1a659952",
+    "url": "/CliqueEntertainment/static/js/runtime~main.3bc3632e.js"
+  },
+  {
+    "revision": "1568d8fca01ce90c0673",
+    "url": "/CliqueEntertainment/static/js/main.1568d8fc.chunk.js"
+  },
+  {
+    "revision": "b5ed347c03c340df434c",
+    "url": "/CliqueEntertainment/static/js/2.b5ed347c.chunk.js"
+  },
+  {
+    "revision": "1568d8fca01ce90c0673",
+    "url": "/CliqueEntertainment/static/css/main.3e2d72fc.chunk.css"
+  },
+  {
+    "revision": "2137edab86e5d82b2afba63b16c4f353",
+    "url": "/CliqueEntertainment/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.31ca2292e1ca15b1b62b445eda72e8aa.js"
+  "/CliqueEntertainment/precache-manifest.4c0299a6f85115c203a2f5e370d887da.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/CliqueEntertainment/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
